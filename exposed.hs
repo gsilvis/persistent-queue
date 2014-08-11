692b013dbdebe7b2d163d9af417654effb474da0
George Silvis, III <george.iii.silvis@gmail.com>
2014-08-11 17:46:17 +0000

    Change handling of final level.
    The level is now NOW allowed to be empty.  This makes the entirely empty queue
    a special case.  This caused some changes in the rest of the code as well.

--- exposed.hs
+++ exposed.hs
@@ -86,11 +86,13 @@ data L3 a :: * -> * -> * -> * -> * -> * -> * -> * where
          L3 a mbot bot lreq lexp rreq rm1 Leftist ->
          L3 a top bot lreq lexp rreq rexp Rightist
 
+data Final c = Final1 c
+             | Final2 c c
+             | Final3 c c c
+
 data L4 a :: * -> * -> * -> * where
-  L4E0 :: () ->
-          L4 a top lexposure rexposure
-  L4E1 :: NLayered top Pair a ->
-          L4 a top lexposure rexposure
+  L4E :: Final (NLayered top Pair a) ->
+         L4 a top lexposure rexposure
   L4 :: Level a top LModifying lmid lexp RModifying rmid rexp ->
         S4 a (Succ top) lmid rmid ->
         L4 a top lexp rexp
@@ -101,20 +103,25 @@ data S4 a :: * -> * -> * -> * where
         L4 a bot lbot rbot ->
         S4 a top ltop rtop
 
-data Queue a = Q00 (S4 a Zero L0Exposed R0Exposed)
-             | Q02 (S4 a Zero L0Exposed R2Exposed)
-             | Q20 (S4 a Zero L2Exposed R0Exposed)
-             | Q22 (S4 a Zero L2Exposed R2Exposed)
+data Queue' a :: * -> * -> * -> * where
+  Q0 :: Queue' a n lexposure rexposure
+  QN :: S4 a n lexposure rexposure ->
+        Queue' a n lexposure rexposure
+
+data Queue a = Q00 (Queue' a Zero L0Exposed R0Exposed)
+             | Q02 (Queue' a Zero L0Exposed R2Exposed)
+             | Q20 (Queue' a Zero L2Exposed R0Exposed)
+             | Q22 (Queue' a Zero L2Exposed R2Exposed)
 
 empty :: Queue a
-empty = Q00 (S4 L1E L3E (L4E0 ()))
+empty = Q00 Q0  --Q00 (S4 L1E L3E (L4E0 ()))
 
 singleton :: a -> Queue a
-singleton a = Q00 (S4 L1E L3E (L4E1 (L0 a)))
+singleton a = Q00 (QN (S4 L1E L3E (L4E (Final1 (L0 a)))))
 
 doubleton :: a -> Queue a
-doubleton a =
-  Q00 (S4 (L1L (Level (LH1 (L0 a)) (RH1 (L0 a))) L1E) L3E (L4E0 ()))
+doubleton a = Q00 (QN (S4 L1E L3E (L4E (Final2 (L0 a) (L0 a)))))
+--  Q00 (S4 (L1L (Level (LH1 (L0 a)) (RH1 (L0 a))) L1E) L3E (L4E0 ()))
 
 
 bestowL :: L1 a top mid ->
@@ -146,10 +153,10 @@ pushlevel (LevelMU level) body = bestowL L1E level body
 pushlevel (LevelMM (Level l r)) body = S4 L1E L3E (L4 (Level l r) body)
 
 
-npushl :: NLayered n Pair a ->
-          S4 a n L0Exposed rexposure ->
-          S4 a n L2Exposed rexposure
-npushl a (S4 l1 l3 l4) = case l1 of
+npushl' :: NLayered n Pair a ->
+           S4 a n L0Exposed rexposure ->
+           S4 a n L2Exposed rexposure
+npushl' a (S4 l1 l3 l4) = case l1 of
   L1L (Level (LH1 l) (RH1 r)) m1 ->
     pushlevel (LevelMU (Level (LH2 (Pair a l)) (RH1 r))) (S4 m1 l3 l4)
   L1E -> case l3 of
@@ -166,29 +173,37 @@ npushl a (S4 l1 l3 l4) = case l1 of
     L3E -> case l4 of
       L4 (Level (LH0 ()) r) rest ->
         pushlevel (LevelUM (Level (LH1 a) r)) rest
-      L4E0 () -> S4 L1E L3E (L4E1 a)
-      L4E1 b -> S4 (L1L (Level (LH1 a) (RH1 b)) L1E) L3E (L4E0 ())
+      L4E (Final1 b) -> S4 L1E L3E (L4E (Final2 a b))
+      L4E (Final2 b c) -> S4 L1E L3E (L4E (Final3 a b c))
+      L4E (Final3 l r c) ->
+        S4 (L1L (Level (LH1 a) (RH1 c)) L1E) L3E (L4E (Final1 (LN (Pair l r))))
 
+npushl :: NLayered n Pair a ->
+          Queue' a n L0Exposed rexposure ->
+          Queue' a n L2Exposed rexposure
+npushl a Q0 = QN (S4 L1E L3E (L4E (Final1 a)))
+npushl a (QN q) = QN (npushl' a q)
 
-fix2l :: S4 a n L2Exposed rexposure ->
-         S4 a n L0Exposed rexposure
-fix2l (S4 l1 l3 l4) = case l3 of
+fix2l :: Queue' a n L2Exposed rexposure ->
+         Queue' a n L0Exposed rexposure
+fix2l Q0 = Q0
+fix2l (QN (S4 l1 l3 l4)) = QN $ case l3 of
   L3L (Level (LH2 l) (RH1 r)) m1 m2 m3 ->
-    bestowL l1 (Level (LH0 ()) (RH1 r)) $ npushl (LN l) $ case m2 of
+    bestowL l1 (Level (LH0 ()) (RH1 r)) $ npushl' (LN l) $ case m2 of
       L2LE -> (S4 m1 m3 l4)
       L2LL (Level kl (RH1 kr)) n1 n2 -> 
         S4 m1 (L3L (Level kl (RH1 kr)) n1 n2 m3) l4
   L3R (Level (LH1 kl) kr) m1 m2 m3 -> case m3 of
     L3L (Level (LH2 l) (RH1 r)) n1 n2 n3 -> case n2 of
       L2LL (Level (LH0 ()) (RH1 ir)) o1 o2 ->
-        case npushl (LN l) (S4 n1 (L3L (Level (LH0 ()) (RH1 ir)) o1 o2 n3) l4) of
+        case npushl' (LN l) (S4 n1 (L3L (Level (LH0 ()) (RH1 ir)) o1 o2 n3) l4) of
         S4 p1 p3 p4 -> S4 l1 (L3R (Level (LH1 kl) kr) m1 m2 result) p4 where
           result = case p3 of
             L3E -> L3L (Level (LH0 ()) (RH1 r)) p1 L2LE L3E
             q3@(L3R _ _ _ _) -> L3L (Level (LH0 ()) (RH1 r)) p1 L2LE q3
             (L3L inner q1 q2 q3) ->
               L3L (Level (LH0 ()) (RH1 r)) p1 (L2LL inner q1 q2) q3
-      L2LE -> case npushl (LN l) (S4 n1 n3 l4) of
+      L2LE -> case npushl' (LN l) (S4 n1 n3 l4) of
         S4 o1 o3 o4 -> S4 l1 (L3R (Level (LH1 kl) kr) m1 m2 result) o4 where
           result = case o3 of
             L3E -> (L3L (Level (LH0 ()) (RH1 r)) o1 L2LE L3E)
@@ -198,14 +213,12 @@ fix2l (S4 l1 l3 l4) = case l3 of
     L3E -> case l4 of
       L4 (Level (LH2 l) r) rest ->
         (S4 l1 (L3R (Level (LH1 kl) kr) m1 m2 L3E) (L4 (Level (LH0 ()) r) new))
-          where new = npushl (LN l) rest
-      L4E0 () -> (S4 l1 (L3R (Level (LH1 kl) kr) m1 m2 L3E) (L4E0 ()))
-      L4E1 a -> (S4 l1 (L3R (Level (LH1 kl) kr) m1 m2 L3E) (L4E1 a))
+          where new = npushl' (LN l) rest
+      L4E final -> (S4 l1 (L3R (Level (LH1 kl) kr) m1 m2 L3E) (L4E final))
   L3E -> case l4 of
-    L4 (Level (LH2 l) r) rest -> case npushl (LN l) rest of
+    L4 (Level (LH2 l) r) rest -> case npushl' (LN l) rest of
       new -> S4 l1 L3E (L4 (Level (LH0 ()) r) new)
-    L4E0 () -> S4 l1 L3E (L4E0 ())
-    L4E1 a -> S4 l1 L3E (L4E1 a)
+    L4E final -> S4 l1 L3E (L4E final)
 
 
 pushl :: a -> Queue a -> Queue a
